c9aacfa4c2f26efae12f298736846a9de93e8d3f
Esoriak <somahaut.dev@gmail.com>
2019-03-28 17:06:59 +0100

    add props destination

--- src/Components/Travel.js
+++ src/Components/Travel.js
@@ -5,6 +5,7 @@ const Travel = ({destination,country, photo, distance}) => (
 <div> <p className="destination">{destination}</p> <p className="country">{country}</p> 
 
 <img className="photodeville" src={photo}/>
+<p>{distance}</p>
 </div>
 );
 export default Travel
